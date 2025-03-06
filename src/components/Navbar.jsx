984356c496e0705f1c1c7675d91497be74e78902
Ivan <contaivanrodrigues@gmail.com>
2025-03-06 11:47:12 -0300

    Estrutura o projeto

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/"><BiCameraMovie/> MoviesLib</Link>
+      </h2>
+      <form>
+        <input type="text" placeholder="Busque um filme" />
+        <button type="submit"><BiSearchAlt2/></button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
