8ed5f0fcef8b25170557cda7a5562af1ba1b3239
Ivan <contaivanrodrigues@gmail.com>
2025-03-07 11:54:42 -0300

    consuma da api e criação do componente movieCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-
-
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
   return (
-    <div className='App'>
-      <Navbar/>
-      <h2>Movies Lib</h2>
-      
-       <Outlet/>
+    <div className="App">
+      <Navbar />
+
+      <Outlet />
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY; // Corrigido o nome da variável
+
 const Home = () => {
-    return ( <div>Home</div> );
-}
- 
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setTopMovies(data.results);
+    } catch (error) {
+      console.error("Erro ao buscar filmes:", error);
+    }
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?api_key=${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2>Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie} key={movie.id}/>)}
+      </div>
+    </div>
+  );
+};
+
 export default Home;
