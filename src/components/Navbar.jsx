ec82399c00c6744f00740ca9bcea94804690c210
Ivan <contaivanrodrigues@gmail.com>
2025-03-21 10:14:41 -0300

    correçoes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav id="navbar">
       <h2>
         <Link to="/">
-          <BiCameraMovie /> MoviesLib
+          <BiCameraMovie /> <p>MoviesLib</p>
         </Link>
       </h2>
       <form onSubmit={handleSubmit}>
